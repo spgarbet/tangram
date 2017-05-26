9b29b1ddc2c6fd21df308437864c1ccbc55c97d4
Shawn Garbett <shawn@garbett.org>
2017-05-25 21:37:55 -0500

    Detailing GPL-3 licensings and authors

--- R/dplr-latexify.R
+++ R/dplr-latexify.R
@@ -1,6 +1,19 @@
-## latexDate and latexify copied from dplR 1.6.6
-## latexDate and latexify are GPL-3 licensed, Copyright (c) 2017 Andy Bunn
-## Modifications Copyright (c) 2017 Shawn Garbett and GPL-3
+# dplR::latexify 1.6.6 modified for use in tangram
+# Copyright (C) 2017 Andy Bunn
+# Copyright (C) 2017 Shawn Garbett, modifications
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## Return a string representing the given date(s) (default: current date)
 ## in the format used by \today in LaTeX.
