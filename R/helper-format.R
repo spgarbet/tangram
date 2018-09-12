4cb29778a41885455c47a202d0446bbb7f2e3cc4
Shawn Garbett <shawn@garbett.org>
2018-09-12 10:32:53 -0500

    Fixed format guessing bug that can hit infinity for some conditions

--- R/helper-format.R
+++ R/helper-format.R
@@ -1,16 +1,16 @@
 # tangram a general purpose table toolkit for R
 # Copyright (C) 2017 Shawn Garbett
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -65,9 +65,13 @@ format_guess <- function(x)
   d <- x[!is.na(x)]
   if(length(d) == 0) return(0) # Nothing, then just return 0 for rounding
   if(all(d == floor(d)))       # Is it all whole numbers, then no decimals
-    return(0)
+    0
   else
-    # Otherwise use 3 significant digits of a representative smaller side quantile
-    return(max(2-max(floor(log10(quantile(abs(d), c(0.05, 0.5))))), 0))
+  {
+    consider <- quantile(abs(d), c(0.05, 0.5))
+    if(sum(consider) == 0.0) 3 else
+      # Otherwise use 3 significant digits of a representative smaller side quantile
+      max(2-max(floor(log10(consider))), 0)
+  }
 }
 
