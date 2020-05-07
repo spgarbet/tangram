6d40132680d4d414fbe9aa4870fa820ee04b9128
Shawn Garbett <shawn@garbett.org>
2020-05-07 09:45:21 -0500

    Fix for Issue #64 (get(style))

--- R/compile.R
+++ R/compile.R
@@ -419,8 +419,7 @@ tangram.formula <- function(x, data=NULL, id=NULL, transforms=NULL, caption=NULL
   if(!is.null(data) && (length(class(data)) > 1 || !inherits(data,"data.frame"))) stop("data must be supplied as data frame")
   if(is.null(id) && "knitr" %in% .packages()) id <- knitr::opts_current$get("label")
   if(is.null(id)) warning("tangram() will require unique id to be specified in the future")
-  if(is.null(transforms)) transforms <- get(style, envir=globalenv())
-  if(is.null(transforms)) transforms <- get(style)
+  if(is.null(transforms)) transforms <- tryCatch(get(style, envir=globalenv()), finally = get(style))
 
   # Helper function for single transform function
   if(!inherits(transforms, "list"))
